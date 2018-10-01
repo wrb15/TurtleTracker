9b959e9721c0f3e3a8a65d290db065a3515b2eed
wrb15 <42747406+wrb15@users.noreply.github.com>
2018-10-01 12:42:10 -0400

    Display information of a user-specific ARGOS observation

--- ARGOSQueryTool.py
+++ ARGOSQueryTool.py
@@ -37,9 +37,32 @@ for lineString in lineStrings:
     obsLat = lineData[5]                # Observation Latitude
     obsLon = lineData[6]                # Observation Longitude
 
-    # Add values to dictionary
-    dateDict[recordID] = obsDateTime   
-    locationDict[recordID] = (obsLat, obsLon) 
+    #Filter records that get added to the dictionary
+    if obsLC in ("1","2","3"):
+
+        # Add values to dictionary
+        dateDict[recordID] = obsDate
+        locationDict[recordID] = (obsLat, obsLon) 
 
 # Indicate script is complete
 print ("Finished")
+
+# Ask the user for a date, specifying the format
+userDate = input("Enter a date (M/D/YYYY)")
+
+#Create an empty key list
+keyList = []
+
+# Loop through all key, value pairs in the dateDictionary
+for k, v in dateDict.items():
+    #See if the date (the value) matches the user date
+    if v == userDate:
+        keyList.append(k)
+
+# Loop through each key and report the associated date location
+for k in keyList:
+    theDate = dateDict[k]
+    theLocation = locationDict[k]
+    theLat = theLocation[0]
+    theLon = theLocation[1]
+    print("Record {0}: Sara was see at {1}N-{2}W, on {3}".format(k,theLat,theLon,theDate))
